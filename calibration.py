36a625ed29edb1077ab3aa8334368b62fec14a1e
chris <christoph.ostertag@mail.de>
2020-11-28 19:38:16 +0100

    Improved calibration.py

--- calibration.py
+++ calibration.py
@@ -1,4 +1,5 @@
 # IMPORTS
+import sys
 import argparse
 import cv2
 import os
@@ -11,11 +12,13 @@ import torch
 from torchvision.utils import make_grid
 
 # PARSE ARGS
-parser = argparse.ArgumentParser(description='Description of your program')
-parser.add_argument('-v','--verbose', help='Show visual output of calibration', action='store_true')
+parser = argparse.ArgumentParser(description='Calibrates camera from chess pictures located at calibration/thermal/.')
+parser.add_argument('-v','--verbose', help='show visual output of calibration', action='store_true')
+parser.add_argument('-r','--recalibrate', help='recalibrate, even if calibration already exists', action='store_true')
 args = vars(parser.parse_args())
 
 verbose = args['verbose']
+recalibrate = args['recalibrate']
 
 # FUNCTION DEFINITIONS
 def show_images(images: np.ndarray, mask: np.ndarray = None, figsize=None):
@@ -55,49 +58,77 @@ def draw_corners(image, corners, grid, cmap=None):
 
 ### CALIBRATION
 
-# READ IMAGES FOR CALIBRATION
-print("LOADING IMAGES")
-images = []
-for image in glob('calibration/thermal/*.png'):
-    images.append(cv2.imread(image, cv2.IMREAD_GRAYSCALE))  # h x w
-images = np.array(images)  # n x h x w
+if __name__ == '__main__':
 
-if verbose:
-    show_images(images[:16], figsize=(12, 12))
+    # Check if calibration parameters can be loaded
+    finished = False
+    if not recalibrate:
+        try:
+            # LOAD CALIBRATION PARAMETERS
+            dir = os.path.join('calibration', 'parameters')
 
-# checkerboard is of grid type (8, 7), as a convention
-# the longer side is considered the width of the board
-grid = (8, 7)
-square_size = 50  # mm
+            err = np.load(os.path.join(dir,'err.npy'), allow_pickle=False)
+            K = np.load(os.path.join(dir,'K.npy'), allow_pickle=False)
+            dist_coeffs = np.load(os.path.join(dir,'dist_coeffs.npy'), allow_pickle=False)
+            rvecs = np.load(os.path.join(dir,'rvecs.npy'), allow_pickle=False)
+            tvecs = np.load(os.path.join(dir,'tvecs.npy'), allow_pickle=False)
 
-# FIND CORNERS
-print("SEARCHING FOR CHESS GRID CORNERS")
-mask, found_corners = search_for_corners(images, grid, invert=True)
-# Check if find corners failed complety
-assert (mask == 1).any()
-print(f'Found corners successfully on: {mask.sum()}/{len(mask)} images.')
+            print("CALIBRATION PARAMETERS ALREADY EXIST")
+            finished = True
 
-if verbose:
-    draw_corners(images[mask, ...][0], found_corners[0], grid)
+        except:
+            print("CALIBRATION PARAMETERS DO NOT YET EXIST")
+            print("STARTING CALIBRATION")
+    
+    if finished: sys.exit()
 
-image_points = found_corners  # 2d points in image plane
-# 3d point in real world space, z coordinates will stay 0
-world_points = np.zeros((*found_corners.shape[:2], 3), 
-    dtype=found_corners.dtype)
-# create xy world points
-xy = square_size * np.mgrid[:grid[0],:grid[1]].T.reshape(-1, 2)
-world_points[..., :2] = xy
+    if recalibrate: print("STARTING RECALIBRATION")
 
-# CALIBRATE
-print("CALCULATING CAMERA CALIBRATION PARAMETERS")
-err, K, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(world_points, 
-    image_points, images.shape[-2:], None, None)
+    # READ IMAGES FOR CALIBRATION
+    print("LOADING IMAGES")
+    images = []
+    for image in glob('calibration/thermal/*.png'):
+        images.append(cv2.imread(image, cv2.IMREAD_GRAYSCALE))  # h x w
+    images = np.array(images)  # n x h x w
 
-# SAVE PARAMS
-print("SAVING CAMERA CALIBRATION PARAMETERS")
-dir = os.path.join('calibration','parameters')
+    if verbose:
+        show_images(images[:16], figsize=(12, 12))
 
-for par, par_name in zip([err, K, dist_coeffs, rvecs, tvecs],['err', 'K', 'dist_coeffs', 'rvecs', 'tvecs']):
-    np.save(os.path.join(dir,par_name), par, allow_pickle=False)
+    # checkerboard is of grid type (8, 7), as a convention
+    # the longer side is considered the width of the board
+    grid = (8, 7)
+    square_size = 50  # mm
 
-print("FINISHED CALIBRATION")
+    # FIND CORNERS
+    print("SEARCHING FOR CHESS GRID CORNERS")
+    mask, found_corners = search_for_corners(images, grid, invert=True)
+    # Check if find corners failed complety
+    assert (mask == 1).any()
+    print(f'Found corners successfully on: {mask.sum()}/{len(mask)} images.')
+
+    if verbose:
+        draw_corners(images[mask, ...][0], found_corners[0], grid)
+
+    image_points = found_corners  # 2d points in image plane
+    # 3d point in real world space, z coordinates will stay 0
+    world_points = np.zeros((*found_corners.shape[:2], 3), 
+        dtype=found_corners.dtype)
+    # create xy world points
+    xy = square_size * np.mgrid[:grid[0],:grid[1]].T.reshape(-1, 2)
+    world_points[..., :2] = xy
+
+    # CALIBRATE
+    print("CALCULATING CAMERA CALIBRATION PARAMETERS")
+    err, K, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(world_points, 
+        image_points, images.shape[-2:], None, None)
+
+    # SAVE PARAMS
+    print("SAVING CAMERA CALIBRATION PARAMETERS")
+    dir = os.path.join('calibration','parameters')
+
+    if not os.path.exists(dir): os.mkdir(dir)
+
+    for par, par_name in zip([err, K, dist_coeffs, rvecs, tvecs],['err', 'K', 'dist_coeffs', 'rvecs', 'tvecs']):
+        np.save(os.path.join(dir,par_name), par, allow_pickle=False)
+
+    print("FINISHED CALIBRATION")
--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,38 @@
 import numpy as np
 import os
+from glob import glob
+from typing import List, Dict, Tuple
 
-# LOAD CALIBRATION PARAMETERS
-dir = os.path.join('calibration', 'parameters')
+# DEFINE FUNCTIONS
 
-err = np.load(os.path.join(dir,'err.npy'), allow_pickle=False)
-K = np.load(os.path.join(dir,'K.npy'), allow_pickle=False)
-dist_coeffs = np.load(os.path.join(dir,'dist_coeffs.npy'), allow_pickle=False)
-rvecs = np.load(os.path.join(dir,'rvecs.npy'), allow_pickle=False)
-tvecs = np.load(os.path.join(dir,'tvecs.npy'), allow_pickle=False)
+# bounding boxes are not always axis aligned
+def aabb(polys: List[np.ndarray]) -> np.ndarray:
+    bboxes = np.empty((len(polys), 4), dtype=np.float32)
+    for i, poly in enumerate(polys):  # height, width
+        # poly given as (x, y) points
+        x = poly[:, 0]
+        y = poly[:, 1]
+        xmin, ymin = x.min(), y.min()
+        xmax, ymax = x.max(), y.max()
+        width = xmax - xmin
+        height = ymax - ymin
+        bboxes[i, :] = (xmin, ymin, width, height)
+    return bboxes
+
+
+def annotate_aabb(image, bboxes, color=(0, 255, 0)):
+    image = image[..., None].repeat(3, axis=-1)  # rgb image
+    for bbox in bboxes:
+        image = cv2.rectangle(image, bbox, 
+            color=color, thickness=2)
+    return image  # annotation drawn on top
+
+if __name__ == '__main__':
+    # LOAD CALIBRATION PARAMETERS
+    dir = os.path.join('calibration', 'parameters')
+
+    err = np.load(os.path.join(dir,'err.npy'), allow_pickle=False)
+    K = np.load(os.path.join(dir,'K.npy'), allow_pickle=False)
+    dist_coeffs = np.load(os.path.join(dir,'dist_coeffs.npy'), allow_pickle=False)
+    rvecs = np.load(os.path.join(dir,'rvecs.npy'), allow_pickle=False)
+    tvecs = np.load(os.path.join(dir,'tvecs.npy'), allow_pickle=False)
