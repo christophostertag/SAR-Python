4a7338513ba8819f95dd672ae402dc2740f5caaa
chris <christoph.ostertag@mail.de>
2022-01-23 19:09:24 +0100

    bugfix

--- temporal_diff/preprocessing.py
+++ temporal_diff/preprocessing.py
@@ -1,29 +1,22 @@
 from tqdm import tqdm
 import numpy as np
 
-def equalize_color_distribution(image_sets):
+def equalize_color_distribution(images):
 
-    print("preprocessing images started")
+    # 1. select each image
+    for _ in images:
+        for image in _:
+            # 2. find 25% and 75% percentile over this image on the three color channels
+            p25 = np.percentile(image, 25, axis=(0,1))
+            p75 = np.percentile(image, 75, axis=(0,1))
+            # 3. change color distribution
+            # 3.1 set 25% percentile to 0 and set 75% percentile to 1
+            image[:,:,0] = (image[:,:,0] - p25[0]) / (p75[0]-p25[0])
+            image[:,:,1] = (image[:,:,1] - p25[1]) / (p75[1]-p25[1])
+            image[:,:,2] = (image[:,:,2] - p25[2]) / (p75[2]-p25[2])
+            # 4. Check if it worked
+            # print()
+            # print(np.percentile(image, 25, axis=(0,1)))
+            # print(np.percentile(image, 75, axis=(0,1)))
 
-    for images, paths, boxes in tqdm(image_sets):
-        # 1. select each image
-        for _ in images:
-            for image in _:
-                # 2. find 25% and 75% percentile over this image on the three color channels
-                p25 = np.percentile(image, 25, axis=(0,1))
-                p75 = np.percentile(image, 75, axis=(0,1))
-                # 3. change color distribution
-                # 3.1 set 25% percentile to 0 and set 75% percentile to 1
-                image[:,:,0] = (image[:,:,0] - p25[0]) / (p75[0]-p25[0])
-                image[:,:,1] = (image[:,:,1] - p25[1]) / (p75[1]-p25[1])
-                image[:,:,2] = (image[:,:,2] - p25[2]) / (p75[2]-p25[2])
-                # 4. Check if it worked
-                # print()
-                # print(np.percentile(image, 25, axis=(0,1)))
-                # print(np.percentile(image, 75, axis=(0,1)))
-
-        pass
-
-    print("preprocessing images finished")
-
-    return images, paths, boxes
+    return images
--- temporal_diff/temporal_diff.py
+++ temporal_diff/temporal_diff.py
@@ -258,10 +258,15 @@ def main(
     for i in range(skip):
         next(image_sets)
 
-    image_sets = equalize_color_distribution(image_sets)
-    print("Images sets color distribution equalized")
-
     for images, paths, boxes in tqdm(image_sets):
+
+        images = equalize_color_distribution(images)
+        print("Images color distribution equalized")
+
+        # check that equalization worked
+        # print(np.percentile(images[0,0], 25, axis=(0,1))) # should be three 0s
+        # print(np.percentile(images[0,0], 75, axis=(0,1))) # should be three 1s
+
         p = Paths.output / paths[3, 4]
         p.parent.mkdir(parents=True, exist_ok=True)
         try:
