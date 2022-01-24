cee98309db1d9a66d804df23707005120f3b9877
Markus <ma.steindl@gmail.com>
2022-01-24 13:06:43 +0100

    Fix

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from temporal_diff.temporal_diff import main
 if __name__ == '__main__':
     warnings.filterwarnings("ignore", message="Mean of empty slice")
     warnings.filterwarnings("ignore", message="divide by zero encountered")
-    # main(dataset='val')
+    main(dataset='val')
     main(dataset='test')
